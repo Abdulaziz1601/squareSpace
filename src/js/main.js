981a56d07d498c3ff30d1278e279168d485095db
abdulaziz <abdulazizfaactory@gmail.com>
2021-02-26 10:43:23 +0500

    Templates section started

--- src/js/main.js
+++ src/js/main.js
@@ -6,13 +6,19 @@ $(document).ready(function(){
         speed: 500,
         fade: true,
         nextArrow: '<button class="slick-next slick-btn"><img src="icons/nextArrow.svg" alt=""> </button>',
-        prevArrow: '<button class="slick-prev slick-btn"><img src="icons/prevArrow.svg" alt=""> </button>'
-
+        prevArrow: '<button class="slick-prev slick-btn"><img src="icons/prevArrow.svg" alt=""> </button>',
+        // onInit: function(){
+        // $('.current').text('1');
+        // },
+        // onAfterChange: function(){
+        // $('.current').text($('.info__slick-slide').slickCurrentSlide()+1);
+        // }
+  
     });
    
 
   });
 
-let currentSlide = $('.info__slick-slide').slick('slick-current');
-const currentIndex = document.querySelector('.info__current');
-currentIndex.innerHTML = `${currentSlide}`;
+// let currentSlide = $('.info__slick-slide').slick('slick-current');
+// const currentIndex = document.querySelector('.info__current');
+// currentIndex.innerHTML = `${currentSlide}`;
