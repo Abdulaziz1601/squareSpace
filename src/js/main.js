b913ca2c6aeaf345c62048e3742ae49ba4b10eaa
abdulaziz <abdulazizfaactory@gmail.com>
2021-02-26 19:48:28 +0500

    Slider Slick optimization

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,23 @@ $(document).ready(function(){
         // }
   
     });
+
+
+    let currentSlide = jQuery(slick.$slides.get(currentSlide)).find('.info__slick-slide').data('caption');
+    const currentIndex = document.querySelector('.info__current');
+    currentIndex.innerHTML = `${currentSlide}`;
+
+    
+
+
+    $('.templates__slick-slide').slick({
+      // autoplay: true,
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      nextArrow: '<button class="slick-next slick-btn"><img src="icons/nextArrow.svg" alt=""> </button>',
+      prevArrow: '<button class="slick-prev slick-btn"><img src="icons/prevArrow.svg" alt=""> </button>',
+  });
    
 
   });
