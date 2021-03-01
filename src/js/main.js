c1aa78a3be1067e37d08bfa158ca65971c487f7b
abdulaziz <abdulazizfaactory@gmail.com>
2021-03-02 02:34:37 +0500

    Learnt how to make img responsible and start section done

--- src/js/main.js
+++ src/js/main.js
@@ -7,35 +7,15 @@ $(document).ready(function(){
         fade: true,
         nextArrow: '<button class="slick-next slick-btn"><img src="icons/nextArrow.svg" alt=""> </button>',
         prevArrow: '<button class="slick-prev slick-btn"><img src="icons/prevArrow.svg" alt=""> </button>',
-        // onInit: function(){
-        // $('.current').text('1');
-        // },
-        // onAfterChange: function(){
-        // $('.current').text($('.info__slick-slide').slickCurrentSlide()+1);
-        // }
-  
     });
 
-
-    let currentSlide = jQuery(slick.$slides.get(currentSlide)).find('.info__slick-slide').data('caption');
-    const currentIndex = document.querySelector('.info__current');
-    currentIndex.innerHTML = `${currentSlide}`;
-
-    
-
-
     $('.templates__slick-slide').slick({
-      // autoplay: true,
+      autoplay: true,
       infinite: true,
       slidesToShow: 2,
       slidesToScroll: 1,
       nextArrow: '<button class="slick-next slick-btn"><img src="icons/nextArrow.svg" alt=""> </button>',
       prevArrow: '<button class="slick-prev slick-btn"><img src="icons/prevArrow.svg" alt=""> </button>',
-  });
+    });
    
-
   });
-
-// let currentSlide = $('.info__slick-slide').slick('slick-current');
-// const currentIndex = document.querySelector('.info__current');
-// currentIndex.innerHTML = `${currentSlide}`;
